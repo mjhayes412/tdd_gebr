e846be4b063c6590ca86ecc9a3ed95ea02e51d1c
mjh6732 <michael_j_hayes@homedepot.com>
2017-06-17 09:43:09 -0400

    extend functionaltest with user stories  Please enter the commit message for your changes. Lines starting

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,25 @@
+import unittest
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+
+class NewVistorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_starting_a_new_todo_list(self):
+        # Agatha has heard about the new gebr site
+        # She goes to its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention bulldogrescue
+        self.assertIn('bulldogrescue', self.browser.title)
+
+        # Satisfied, she goes back to sleep
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
