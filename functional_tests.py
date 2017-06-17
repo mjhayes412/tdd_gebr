388e88d8157500aea2da18307af324247bad1ec4
mjh6732 <michael_j_hayes@homedepot.com>
2017-06-17 12:29:50 -0400

    refactor functional_test for DRY

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVistorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_starting_a_new_todo_list(self):
         # Agatha has heard about the new gebr site
         # She goes to its homepage
@@ -37,27 +42,19 @@ class NewVistorTest(unittest.TestCase):
         # "1: Look for food" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Look for food', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Look for food')
 
         # There is still a text box inviting her to add another item. She
         # enters "Sit near food bowl and wait" (Agatha is very
         # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys('Sit near food bowl and wait')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Look for food')
+        self.check_for_row_in_list_table('2: Sit near food bowl and wait')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
