100c1c99be964f4d3881ec3c842d9d3c71720001
mjh6732 <michael_j_hayes@homedepot.com>
2017-06-17 11:15:52 -0400

    Refactor home page view to use a template

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<html><title>George English Bulldog Rescue</title></html>')
+    return render(request, 'home.html')
