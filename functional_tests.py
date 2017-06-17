037b4cc9ad29fa31a1b649d430ab9861ee56963b
mjh6732 <michael_j_hayes@homedepot.com>
2017-06-17 12:17:58 -0400

    refactor functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -29,24 +29,21 @@ class NewVistorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+        # She types "Look for food" into a text box (Agatha's hobby
+        # is eating)
+        inputbox.send_keys('Look for food')
 
         # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
+        # "1: Look for food" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Look for food', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very
+        # enters "Use peacock feathers to make a fly" (Agatha is very
         # methodical)
         self.fail('Finish the test!')
 
