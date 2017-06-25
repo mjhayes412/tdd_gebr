64db3907e9a3c63d4d55202ffa1aebdf2e2e72d8
mjh6732 <michael_j_hayes@homedepot.com>
2017-06-25 12:27:54 -0400

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/forms.py
+++ lists/forms.py
@@ -6,17 +6,10 @@ EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
 
-    item_text = forms.CharField(
-        widget=forms.fields.TextInput(attrs={
-            'placeholder': 'Enter a to-do item',
-            'class': 'form-control input-lg',
-        }),
-    )
-
     class Meta:
         model = Item
         fields = ('text',)
-        widget = {
+        widgets = {
             'text': forms.fields.TextInput(attrs={
                 'placeholder': 'Enter a to-do item',
                 'class': 'form-control input-lg',
